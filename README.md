ce5b65002d988cbcf47fe5de88f43fcefcc89633
Ray Andrew <raydreww@gmail.com>
2019-07-02 17:13:57 +0700

    initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# ucare-research
---
+++ hdfs/install-jdk7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# creating dirs
+mkdir temp-jdk
+
+# download zulu
+wget https://cdn.azul.com/zulu/bin/zulu7.29.0.5-ca-jdk7.0.222-linux_amd64.deb -O temp-jdk/jdk7.deb
+
+# install deps
+sudo apt-get update && sudo apt-get --fix-broken install \
+  java-common \
+  libasound2 \
+  libasound2-data \
+  libxi6 \
+  libxrender1 \
+  libxtst6 \
+  x11-common \
+  libfontconfig1 \
+  fontconfig-config
+
+# install jdk7
+sudo dpkg -i temp-jdk/jdk7.deb
