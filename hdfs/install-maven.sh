eb9bc93c6340f8953917b12625571ce577758155
Ray Andrew <raydreww@gmail.com>
2019-07-02 23:14:03 +0700

    add hadoop scripts

--- hdfs/install-maven.sh
+++ hdfs/install-maven.sh
@@ -10,7 +10,8 @@ MAVEN_VERSION="3.6.1"
 JAVA_DIR="zulu-7-amd64"
 DEST_FOLDER="/opt"
 M2_HOME="/opt/maven"
-BASHRC="$HOME/.bashrc"
+# BASHRC="$HOME/.bashrc"
+MAVEN_PROFILE="/etc/profile.d/maven.sh"
 
 sudo rm -rf /opt/maven && sudo rm -rf /opt/apache-maven-$MAVEN_VERSION
 
@@ -28,27 +29,27 @@ sudo tar xvf $TEMP_FOLDER/maven-$MAVEN_VERSION.tar.gz -C $DEST_FOLDER
 sudo ln -s /opt/apache-maven-$MAVEN_VERSION /opt/maven
 
 # create profile
-check_var_exist $JAVA_HOME $(echo "export JAVA_HOME=/usr/lib/jvm/$JAVA_DIR" >>$BASHRC)
+check_var_exist $JAVA_HOME $(echo "export JAVA_HOME=/usr/lib/jvm/$JAVA_DIR" >>$MAVEN_PROFILE)
 check_var_exist $M2_HOME \
   $(
-    echo "export M2_HOME=${M2_HOME}" >>$BASHRC
-    echo "export JAVA_HOME=/usr/lib/jvm/$JAVA_DIR" >>$BASHRC
-    echo "export PATH=${M2_HOME}/bin:${PATH}" >>$BASHRC
+    echo "export M2_HOME=${M2_HOME}" >>$MAVEN_PROFILE
+    echo "export JAVA_HOME=/usr/lib/jvm/$JAVA_DIR" >>$MAVEN_PROFILE
+    echo "export PATH=${M2_HOME}/bin:${PATH}" >>$MAVEN_PROFILE
   )
 
 # chmod
-# sudo chmod +x $MAVEN_PROFILE
+sudo chmod +x $MAVEN_PROFILE
 
 # source file
-source $BASHRC
+source $MAVEN_PROFILE
 
 # # check installation
-check_program mvn || {
-  echo >&2 "Maven program not found.  Aborting."
-  exit 1
-}
+# check_program mvn || {
+#   echo >&2 "Maven program not found.  Aborting."
+#   exit 1
+# }
 
-echo "Maven installation succeed!"
+# echo "Maven installation succeed!"
 
 # remove tmp
 rm -rf $TEMP_FOLDER
