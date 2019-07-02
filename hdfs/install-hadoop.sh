7bc4e80afe45072a58391986766ebac372114088
Ray Andrew <raydreww@gmail.com>
2019-07-02 23:50:55 +0700

    add hadoop installation

--- hdfs/install-hadoop.sh
+++ hdfs/install-hadoop.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+source "$DIR/_utils.sh"
+
+cd "$DIR/source"
+
+# check installation
+check_program mvn || {
+  echo >&2 "Maven program not found.  Aborting."
+  exit 1
+}
+
+# Compiling maven
+echo "Compiling Hadoop"
+mvn package -Pdist -DskipTests -Dtar
+echo "Hadoop has been compiled!"
