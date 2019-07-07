ded270bc38de1b54b120e833186bdcaf989a9514
Ray Andrew <raydreww@gmail.com>
2019-07-07 13:55:38 +0700

    add custom log dir and cassandra

--- hdfs/run-hadoop.sh
+++ hdfs/run-hadoop.sh
@@ -10,8 +10,10 @@
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
-export HADOOP_HOME="/mnt/extra/source/hadoop-dist/target/hadoop-2.7.1"
-export HADOOP_CONF_DIR="/mnt/extra/source/hadoop-dist/target/hadoop-2.7.1/etc/hadoop"
+$PROJ_DIR="/mnt/extra/ucare-research"
+
+export HADOOP_HOME="$PROJ_DIR/source/hadoop-dist/target/hadoop-2.7.1"
+export HADOOP_CONF_DIR="$PROJ_DIR/source/hadoop-dist/target/hadoop-2.7.1/etc/hadoop"
 export HADOOP_LOG_DIR="/mnt/extra/logs/master"
 
 JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"
