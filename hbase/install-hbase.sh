b9e791bbc5203bb3c59e0a331ea5262b0ac0cbb9
Ray Andrew <raydreww@gmail.com>
2019-07-09 23:33:39 +0700

    [WIP][FEATS] add the extraction script

--- hbase/install-hbase.sh
+++ hbase/install-hbase.sh
@@ -7,6 +7,12 @@ source "$CURRENT_DIR/_utils.sh"
 
 cd "$CURRENT_DIR/source"
 
+export HBASE_VERSION="1.0.4"
+export HBASE_HOME="$CURRENT_DIR/hbase-home"
+export PATH=$HBASE_HOME/bin:$PATH
+
+mkdir -p $HBASE_HOME
+
 # check installation
 check_program mvn || {
   echo >&2 "Maven program not found.  Aborting."
@@ -17,4 +23,5 @@ check_program mvn || {
 echo "Compiling HBase"
 # mvn clean -Dhttps.protocols=TLSv1.2 package -DskipTests
 mvn clean install -Dhttps.protocols=TLSv1.2 -DskipTests assembly:single
+tar xzvf hbase-assembly/target/hbase-$CURRENT_DIR-SNAPSHOT-bin.tar.gz -C $HBASE_HOME
 echo "HBase has been compiled!"
