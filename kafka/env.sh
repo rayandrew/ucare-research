653ba85d86750fe6ee3eeb4b58eb6b7b45ca80a0
Ray Andrew <raydreww@gmail.com>
2019-08-05 12:27:20 +0700

    [WIP][FEATS] edit path

--- kafka/env.sh
+++ kafka/env.sh
@@ -1,2 +1,3 @@
 export GRADLE_HOME=/opt/gradle/gradle-5.5.1
 export PATH=${GRADLE_HOME}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
