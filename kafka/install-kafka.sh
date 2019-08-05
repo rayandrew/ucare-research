09fffe08599bb7994801db56fed846a59b27775a
Ray Andrew <raydreww@gmail.com>
2019-08-05 11:55:57 +0700

    [WIP][FEATS] add kafka installation

--- kafka/install-kafka.sh
+++ kafka/install-kafka.sh
@@ -7,21 +7,21 @@ source "$CURRENT_DIR/_utils.sh"
 
 cd "$CURRENT_DIR/source"
 
-export HBASE_VERSION="1.0.4"
-export HBASE_HOME="$CURRENT_DIR/hbase-home"
-export PATH=$HBASE_HOME/bin:$PATH
+export KAFKA_VERSION="1.0.4"
+# export KAFKA_HOME="$CURRENT_DIR/hbase-home"
+# export PATH=$HBASE_HOME/bin:$PATH
 
-mkdir -p $HBASE_HOME
+# mkdir -p $HBASE_HOME
 
 # check installation
-check_program mvn || {
-  echo >&2 "Maven program not found.  Aborting."
+check_program gradle || {
+  echo >&2 "Gradle program not found.  Aborting."
   exit 1
 }
 
 # Compiling HBase
-echo "Compiling HBase"
+echo "Compiling Kafka"
 # mvn clean -Dhttps.protocols=TLSv1.2 package -DskipTests
-mvn clean install -Dhttps.protocols=TLSv1.2 -DskipTests assembly:single
-tar xzvf hbase-assembly/target/hbase-$HBASE_VERSION-SNAPSHOT-bin.tar.gz -C $HBASE_HOME
-echo "HBase has been compiled!"
+gradle
+gradle jar
+echo "Kafka has been compiled!"
