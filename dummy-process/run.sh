1a8f018578d5087c1e9213833bf86534670bd227
Ray Andrew <raydreww@gmail.com>
2019-08-07 14:40:00 +0700

    edit nohup runner

--- dummy-process/run.sh
+++ dummy-process/run.sh
@@ -8,7 +8,7 @@ cmd=$1
 echo "Starting multiple programs"
 
 for ((i = 0; i < $cmd; i++)); do
-  logDir=./logs/$i nohup java -jar build/libs/dummy-process-fat.jar &
+  logDir=./logs/$i nohup java -jar build/libs/dummy-process-fat.jar >/dev/null 2>&1 &
 done
 
 echo "Stopping multiple programs"
