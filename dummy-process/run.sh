956053b21452024bae0865ba8f5f4de26ff83567
Ray Andrew <raydreww@gmail.com>
2019-08-07 15:00:44 +0700

    remove logs folder in run.sh

--- dummy-process/run.sh
+++ dummy-process/run.sh
@@ -8,6 +8,9 @@ cmd=$1
 echo "Kill all Java Process"
 kill -9 $(ps aux | grep "java" | grep -v 'grep' | awk '{print $2}')
 
+echo "Remove logs folder"
+rm -rf ./logs/*
+
 echo "Starting multiple programs"
 
 for ((i = 0; i < $cmd; i++)); do
