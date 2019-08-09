027ec28d1220116b6818bf9eda2d97b5f437f928
Ray Andrew <raydreww@gmail.com>
2019-08-09 13:37:37 +0700

    add string dedup

---
+++ string-dedup/env.sh
@@ -0,0 +1,4 @@
+export GRADLE_HOME=/opt/gradle/gradle-5.5.1
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export GRADLE_USER_HOME=/mnt/extra/.gradle
+export PATH=${GRADLE_HOME}/bin:${JAVA_HOME}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
